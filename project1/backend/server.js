d87da98cd9085cd867e50c8ca419c6db603323b3
Vaibhav kumar Jha <vaibhavkumarjha053@gmail.com>
2026-07-16 16:09:10 +0530

    Update server.js

--- project1/backend/server.js
+++ project1/backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || 'https://event-booking-frontend-zhg8.onrender.com/',
     credentials: true,
   })
 );
