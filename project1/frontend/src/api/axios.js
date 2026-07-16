1afc96d188e29fab59abce677bf9a95548395f67
Vaibhav kumar Jha <vaibhavkumarjha053@gmail.com>
2026-07-16 19:22:34 +0530

    Update axios.js

--- project1/frontend/src/api/axios.js
+++ project1/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://mern-first-project-7v7t.onrender.com",
+    baseURL: "https://mern-first-project-7v7t.onrender.com/api",
     withCredentials: true
 });
 
